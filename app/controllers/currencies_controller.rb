c20fc7cd8fac0ceee352bfc77c335af71e20a80a
apa yu <rx836@hotmail.com>
2021-07-10 10:34:27 +0800

    Add currency new and create action

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,4 +4,24 @@ class CurrenciesController < AdminController
   def index
     @currencies = Currency.all
   end
+
+  def new
+    @new_currency = Currency.new
+  end
+
+  def create
+    @new_currency = Currency.new(currency_params)
+
+    if @new_currency.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def currency_params
+    params.require(:currency).permit(:code)
+  end
 end
